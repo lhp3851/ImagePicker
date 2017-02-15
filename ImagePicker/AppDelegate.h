f80471752b7008db17391f1807ec726f471b8553
lhp3851 <lhp3851@163.com>
2017-02-15 16:53:42 +0800

    Initial Commit

---
+++ ImagePicker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImagePicker
+//
+//  Created by LiuXuan on 2017/2/15.
+//  Copyright © 2017年 Xuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
